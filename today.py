1ae0d4c92d064b8df129103dbca2a491166b5533
Merve Demirci <mervedemirci.034@icloud.com>
2025-12-17 00:59:10 +0300

    Implement my_buddy_is function

--- today.py
+++ today.py
@@ -5,3 +5,6 @@ def my_name_is():
     """Returns my GitHub username as a string."""
 
     return "MerveDemirci034"
+def my_buddy_is():
+    """Returns my buddy's name as a string."""
+    return "MerveDemirci034"
